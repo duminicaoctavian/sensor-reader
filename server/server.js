6fcd814e43023f1971048fd35f2abf2739bf8482
DESKTOP-K5G7K9R\Octavian <duminicaoctavian@gmail.com>
2020-01-12 17:57:34 +0200

    Removed bluetooth-serial-port dependency

--- server/server.js
+++ server/server.js
@@ -11,38 +11,8 @@ var server = http.createServer(app)
 app.use(bodyParser.json())
 
 server.listen(port, () => {
-	console.log(`Sensor Reader started on port ${port}`)
+	console.log(`PTH Sensor reader started on port ${port}`)
 })
-
-var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort()
-btSerial.inquire()
-
-btSerial.on('found', function (address, name) {
-
-	console.log(`Address: ${address}, name: ${name}`)
-
-	btSerial.findSerialPortChannel(address, function (channel) {
-		btSerial.connect(address, channel, function () {
-			console.log('connected')
-
-			btSerial.write(Buffer.from('my data', 'utf-8'), function (err, bytesWritten) {
-				if (err) console.log(err)
-			})
-
-			btSerial.on('data', function (buffer) {
-				console.log(buffer.toString('utf-8'))
-			});
-		}, function () {
-			console.log('cannot connect');
-		});
-
-		// close the connection when you're ready
-		btSerial.close()
-	}, function () {
-		console.log('Found nothing')
-	})
-})
-
 
 module.exports = {
 	app
