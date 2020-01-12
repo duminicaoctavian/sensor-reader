044dcf5a3aa0c8caf5e9ffc3cf6abf85c5b6e081
DESKTOP-K5G7K9R\Octavian <duminicaoctavian@gmail.com>
2020-01-12 20:24:40 +0200

    Added serialport dependency + read

--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ let config = require('./config/config.js')
 let express = require('express')
 let http = require('http')
 let bodyParser = require('body-parser')
+let SerialPort = require('serialport')
+let Readline = SerialPort.parsers.Readline
+
+const serialport = new SerialPort('COM3')
+
+const parser = new Readline()
+serialport.pipe(parser)
+parser.on('data', console.log)
 
 let port = process.env.PORT
 
